02f967aa34991e328d7560af98d0a2f8a4a2e831
Nuno Araujo <nunobaraujo@hotmail.com>
2018-03-26 20:00:13 +0100

    LWDEV-5188: Emtpy candles bugfix

--- src/Lykke.Job.MtConsistencyChecker/PeriodicalHandlers/ConsistencyCheckHandler.cs
+++ src/Lykke.Job.MtConsistencyChecker/PeriodicalHandlers/ConsistencyCheckHandler.cs
@@ -11,22 +11,16 @@ namespace Lykke.Job.MtConsistencyChecker.PeriodicalHandlers
         private readonly ILog _log;
         private readonly IMonitorService _monitorService;
 
-        private bool _initialCall;
-
         public ConsistencyCheckHandler(ILog log, IMonitorService monitorService) :
             base(nameof(ConsistencyCheckHandler), monitorService.MonitorInterval, log)
         {
             _log = log;
             _monitorService = monitorService;
-            _initialCall = true;
         }
 
         public override async Task Execute()
         {
-            if (_initialCall)
-                _initialCall = false;
-            else
-                await _monitorService.CheckConsistency();
+            await _monitorService.CheckConsistency();
             await _log.WriteInfoAsync(nameof(ConsistencyCheckHandler), null, $"Next Consistency Check:[{DateTime.UtcNow.AddMilliseconds(_monitorService.MonitorInterval):u}]");
         }
     }
